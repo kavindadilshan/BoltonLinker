42fc2ed48fecf770644768b6c09becd986e97ab1
kavindadilshan <kavindadilshan97@gmail.com>
2023-07-29 14:08:48 +0530

    start to create subscribe user store

---
+++ app/src/main/java/com/bolton/oom/notifier/store/impl/SubscrioptionStoreImpl.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.bolton.oom.notifier.store.impl;
+
+import com.bolton.oom.notifier.dto.ResponseDTO;
+import com.bolton.oom.notifier.dto.SubscriptionDetailsDTO;
+import com.bolton.oom.notifier.store.SuperStore;
+
+/**
+ *
+ * @author Kevin Boy
+ */
+public class SubscrioptionStoreImpl implements SuperStore<SubscriptionDetailsDTO>{
+
+    @Override
+    public ResponseDTO save(SubscriptionDetailsDTO t) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public ResponseDTO remove(SubscriptionDetailsDTO t) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public ResponseDTO getAllData() throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public ResponseDTO findBy(SubscriptionDetailsDTO t) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
